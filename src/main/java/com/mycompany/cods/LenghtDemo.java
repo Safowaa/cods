59c3940eb99154c424c923c5f2bd3d214f4f6856
Safowaa <safowaabenedicta@gmail.com>
2022-11-13 15:38:10 +0000

    how to use classes, creating them, assigning values, etc.

---
+++ src/main/java/com/mycompany/cods/LenghtDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cods;
+
+/**
+ *
+ * @author MissSafowaa
+ */
+public class LenghtDemo {
+    public static void main (String[] args ){
+    Rectangle box = new Rectangle();
+    
+    System.out.println("sending the value 10.0 to " 
+                         + "the setLength method.");
+    
+    box.setLength(10.0);
+    
+    System.out.println("Done");
+    }
+    
+}
---
+++ src/main/java/com/mycompany/cods/Rectangle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cods;
+
+/**
+ *
+ * @author MissSafowaa
+ */
+ 
+  public class Rectangle
+  {
+  private double length;
+  private double width;
+ 
+  /**
+  * The setLength method accepts an argument
+  * that is stored in the length field.
+  */
+ 
+  public void setLength(double len)
+  {
+   length = len;
+  }
+  
+  
+   public void setWidth(double w)
+ {
+    width = w;
+ }
+  }
